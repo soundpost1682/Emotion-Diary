3a915ae018cf6f63a3def2ef3e4a70ea83879ffc
evan <guarneri1682@gmail.com>
2024-04-03 16:50:37 -0700

    Routes setting up

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/NotFound";
+
+// 1. "/" : Home
+// 2. "/new" : create a new page
+// 3. "/diary" : view the diary page
+function App() {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>Move to New Page</button>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary' element={<Diary />} />
+        <Route path='*' element={<Notfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+const Notfound = () => {
+  return <div>Something's wrong...</div>;
+};
+
+export default Notfound;
