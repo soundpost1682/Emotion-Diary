a6b9b67a2b71a9db917b8c0dbadf2a2f05e5abae
evan <guarneri1682@gmail.com>
2024-04-04 00:07:02 -0700

    common components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/NotFound";
 import { getEmotionImage } from "./util/get-emotion-image";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 // 1. "/" : Home
 // 2. "/new" : create a new page
@@ -17,7 +19,32 @@ function App() {
   };
   return (
     <>
-      <div>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"< Left"} />}
+        rightChild={<Button text={"Right >"} />}
+      />
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("clicked 123");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"positive"}
+        onClick={() => {
+          console.log("clicked 123");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"negative"}
+        onClick={() => {
+          console.log("clicked 123");
+        }}
+      />
+      {/* <div>
         <img src={getEmotionImage(1)} />
         <img src={getEmotionImage(2)} />
         <img src={getEmotionImage(3)} />
@@ -29,7 +56,7 @@ function App() {
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
       </div>
-      <button onClick={onClickButton}>Move to New Page</button>
+      <button onClick={onClickButton}>Move to New Page</button> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
